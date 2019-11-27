f8368f4daba7992b7bd4b2f90d6cd301c779523d
que2018 <goodiswill2016@gmail.com>
2019-11-27 12:48:17 -0800

    11272019

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 
 App({
   globalData: {
-    version: 'v1.0.0',
+    version: 'v0.2.2',
     is_login: false,
-    email: "",
+    user_id: "",
+    username: "",
     password: "",
+    email: "",
+    first_name: "",
+    last_name: "",
+    address: {},
     API_LOGIN: "https://debug.piggycars.com/api/account/login",
+    API_LOGOUT: "https://debug.piggycars.com/api/account/logout",
+    API_REGISTER: "https://debug.piggycars.com/api/account/register",
+    API_USER: "https://debug.piggycars.com/api/account/user",
+    API_ID: "https://debug.piggycars.com/api/account/register/get_identity",
     API_CARS: "https://debug.piggycars.com/api/catalog/vehicle/get_list",
     API_CAR: "https://debug.piggycars.com/api/catalog/vehicle",
     API_RES: "https://debug.piggycars.com/assets/image"
--- pages/me/index.js
+++ pages/me/index.js
@@ -4,8 +4,18 @@ var util = require('../../utils/util.js');
 
 Page({
   data: {
-    email: "",
+    username: "",
     password: "",
+    first_name: "",
+    last_name: "",
+    email: "",
+    address_1: "",
+    address_2: "",
+    city: "",
+    zone: "",
+    postcode: "",
+    country: "",
+    id_images: [],
     is_login: false,
     show_loading: false
   },
@@ -13,6 +23,10 @@ Page({
     this.setData({ 
       is_login: app.globalData.is_login
     });
+
+    if(this.data.is_login) {
+      this.get_profile();
+    }
   },
   login: function (e) {
     var that = this;
@@ -37,13 +51,117 @@ Page({
         });
 
         if(res.data.success) {
+          app.globalData.is_login = true;
+          wx.setStorageSync("sessionid", res.header["Set-Cookie"]);
+
           that.setData({
-            is_login: true
+            is_login: true,
+            username: "",
+            password: ""
           });
+
+          that.get_profile();
+          that.get_id();
+
         } else {
 
         }
       }
     });   
-  }
+  },
+  get_profile: function (e) {
+    var that = this;
+
+    let header = {
+      'content-type': 'application/x-www-form-urlencoded',
+      'cookie': wx.getStorageSync("sessionid")
+    };
+
+    wx.request({
+      header: header,
+      url: app.globalData.API_USER,
+      complete: function (res) {
+        console.log(res.data);
+
+        if(res.data.success) {
+          app.globalData.user_id = res.data.user_id;
+          app.globalData.email = res.data.email;
+          app.globalData.first_name = res.data.first_name;
+          app.globalData.last_name = res.data.last_name;
+          app.globalData.address = res.data.address;
+
+          that.setData({
+            first_name: res.data.first_name,
+            last_name: res.data.last_name,
+            email: res.data.email,
+            address_1: res.data.address.address_1,
+            address_2: res.data.address.address_2,
+            city: res.data.address.city,
+            zone: res.data.address.zone,
+            postcode: res.data.address.postcode,
+            country: res.data.address.country
+          });
+        } 
+      }
+    });
+  },
+  get_id: function (e) {
+    var that = this;
+
+    let header = {
+      'content-type': 'application/x-www-form-urlencoded',
+      'cookie': wx.getStorageSync("sessionid")
+    };
+
+    wx.request({
+      header: header,
+      url: app.globalData.API_ID,
+      complete: function (res) {
+        console.log(res.data);
+
+        var id_images =[];
+      
+        if(res.data.success) {
+          id_images.push(res.data.data.dl_image);
+          id_images.push(res.data.data.dl_image_back);
+          id_images.push(res.data.data.id_image);
+
+          that.setData({
+            id_images: id_images
+          });
+        }
+
+        console.log(that.data.id_imags);
+      }
+    });
+  },
+  logout: function (e) {
+    var that = this;
+
+    that.setData({
+      loading: true
+    });
+
+    wx.request({
+      url: app.globalData.API_LOGOUT,
+      complete: function (res) {
+        that.setData({
+          loading: false
+        });
+
+        if (res.data.success) {
+          that.setData({
+            is_login: false
+          });
+        } else {
+
+        }
+      }
+    });
+  },
+  clickRegister: function (event) {
+    wx.navigateTo({
+      url: '../register/index'
+    });
+  },
 })
