4a34db6315f6db29f56795748146e25cb1e2319d
que2018 <goodiswill2016@gmail.com>
2019-12-27 17:52:49 -0800

    12272019

--- components/filter/filter.js
+++ components/filter/filter.js
@@ -10,22 +10,36 @@ Component({
     }
   },
   data: {
-    show_filter: false
+    show_filter: false,
+    keys: ["cities", "colors", "dealers", "down_payment", "makes", "mileage", "monthly_payment", "styles", "years"]
   },
   lifetimes: {
     attached: function () {
+      var that = this;
+
       wx.request({
         url: app.globalData.API_FILTERS,
         complete: function (res) {
           console.log(res);
+
+          if(res.data.success) {
+            let filterItems = res.data.data;
+
+            that.setData({
+              filter_items: filterItems
+            });
+
+            for (const key of that.data.keys) {
+              console.log(filterItems[key]);
+            }
+
+          }
         }
       });
     }
   },
   methods: {
     toggleFilter: function (event) {
-      console.log("bind tap ... ");
-
       /* this.animate('#xxxx', [
         { opacity: 1.0, rotate: 0, backgroundColor: '#FF0000' },
         { opacity: 0.5, rotate: 45, backgroundColor: '#00FF00' },
