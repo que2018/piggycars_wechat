ca9e7ce330e21b64cc45286c815fbe1960168d9d
que2018 <goodiswill2016@gmail.com>
2020-08-06 14:38:52 -0700

    08062020

--- miniprogram/pages/community/index.js
+++ miniprogram/pages/community/index.js
@@ -28,20 +28,41 @@ Page({
       data: util.json2Form(data),
       complete: function (res) {
         if (res.data.success) {
-          console.log(res.data);
-
           var top_categories = [];
 
-          if (res.data.top_categories) {
-            for (var i = 0; i < res.data.top_categories.length; i++) {
+          if (res.data.data.top_categories) {
+            for (var i = 0; i < res.data.data.top_categories.length; i++) {
               var top_category = new Object();
 
-              let item = res.data.top_categories[i];
+              let item = res.data.data.top_categories[i];
 
               top_category.id = decodeURIComponent(item.id);
               top_category.title = decodeURIComponent(item.title);
 
-              top_category.posts = item.posts;
+              var posts = [];
+
+              if (item.posts) {
+                for (var j = 0; j < item.posts.length; j++) {
+                 var post = new Object();
+
+                  let post_data = item.posts[j];
+
+                  post.id = decodeURIComponent(post_data.id);
+                  post.title = decodeURIComponent(post_data.title);
+                  post.meta_title = decodeURIComponent(post_data.meta_title);
+                  post.meta_description = decodeURIComponent(post_data.meta_description);
+
+                  if(post_data.image) {
+                    post.image = app.globalData.API_RES_INFO + "/post/sm/" + post_data.image;
+                  } else {
+                    post.image = "../../images/placeholder.jpg";
+                  }
+
+                  posts.push(post);
+                }
+              }
+
+              top_category.posts = posts;
 
               top_categories.push(top_category);
             }
@@ -49,16 +70,22 @@ Page({
 
           var hot_categories = [];
 
-          if (res.data.hot_categories) {
-            for (var i = 0; i < res.data.hot_categories.length; i++) {
+          if (res.data.data.hot_categories) {
+            for (var i = 0; i < res.data.data.hot_categories.length; i++) {
               var hot_category = new Object();
 
-              let item = res.data.hot_categories[i];
+              let item = res.data.data.hot_categories[i];
 
               hot_category.id = decodeURIComponent(item.id);
               hot_category.title = decodeURIComponent(item.title);
               hot_category.background_color = decodeURIComponent(item.background_color);
               hot_category.meta_description = decodeURIComponent(item.meta_description);
+
+              if(item.image) {
+                hot_category.image = app.globalData.API_RES_INFO + "/post_category/orig/" + item.image;
+              } else {
+                hot_category.image = "";
+              }
 
               hot_categories.push(hot_category);
             }
